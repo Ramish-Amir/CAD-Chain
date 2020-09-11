98caa24a97441fc95a997174e3d67f82036fe859
Ramish-Amir <49115590+Ramish-Amir@users.noreply.github.com>
2020-09-11 11:59:38 +0500

    Added Currency MinMax and Recipient Address

--- src/app/buy-crypto/buy-crypto.component.ts
+++ src/app/buy-crypto/buy-crypto.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable, Subscription, throwError} from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import {catchError, retry} from 'rxjs/operators';
 
 @Component({
   selector: 'app-buy-crypto',
@@ -14,52 +14,62 @@ export class BuyCryptoComponent implements OnInit {
   depositCurrency;
   receiveCurrency;
   coinList1: any = [];
-  coinList = ['BTC', 'USDT', 'eth', 'BCH', 'XRP', 'EOS', 'LTC', 'HUSD', 'ETC', 'BSV',
-    'DASH', 'HPT'];
-  coinList2 = [
-    {
-      image: 'https://simpleswap.io/static/img/currencies/A.svg',
-      name: 'Arcblock',
-      symbol: 'eth'
-    },
-    {
-      image: 'https://simpleswap.io/static/img/currencies/ada.svg',
-      name: 'Cardano',
-      symbol: 'ada'
-    },
-    {
-      image: 'https://simpleswap.io/static/img/currencies/xeda.svg',
-      name: 'AdEx',
-      symbol: 'adx'
-    },
-    {
-      image: 'https://simpleswap.io/static/img/currencies/ae.svg',
-      name: 'Aeternity',
-      symbol: 'ae'
-    },
-    {
-      image: 'https://simpleswap.io/static/img/currencies/S.svg',
-      name: 'SingularityNET',
-      symbol: 'agi'
-    }];
+  coinList2: any = [];
+  minMax: any = [];
+  recipientAddress;
+  addressValidation: any = [];
 
-  convertTo(depositCoin, receiveCoin ) {
-    if (this.sendAmount === '0' || this.sendAmount === '' || this.sendAmount === '0.0') {
+  convertTo(depositCoin, receiveCoin) {
+    if (this.sendAmount < this.minMax.min) {
       console.log('amount is not valid');
       this.getAmount.rate = '';
       return;
     }
     const deposit = depositCoin;
-    const receive = 'eth';
+    const receive = receiveCoin;
     const amount = this.sendAmount;
-    return this.http.get('http://127.0.0.1:5000/getRate?deposit=' + deposit + '&receive=' + receive + '&amount=' + amount).subscribe(
+    console.log(receiveCoin);
+    return this.http.get('http://127.0.0.1:5000/getrate?deposit=' + deposit + '&receive=' + receive + '&amount=' + amount).subscribe(
       (data) => {
         this.getAmount = data;
         console.log(data);
       });
   }
+
   getReceiveCoinList(depositCoin) {
-    console.log('get receive coin list function works adn value deposit coin is ' + depositCoin);
+    this.http.get('http://127.0.0.1:5000/currencypair?symbol=' + depositCoin).subscribe(
+      (data) => {
+        this.coinList2 = data;
+        // this.receiveCurrency = this.coinList2[0].symbol;
+        console.log(data);
+        console.log(this.receiveCurrency);
+      }
+    );
+    this.http.get('http://127.0.0.1:5000/getminmax?deposit=' + depositCoin + '&receive=' + this.coinList2[0].symbol).subscribe(
+      (data) => {
+        this.minMax = data;
+        console.log(data);
+      }
+    );
+  }
+  getMinMax(depositCoin, receiveCoin) {
+    console.log(depositCoin);
+    console.log(receiveCoin);
+    console.log(this.receiveCurrency);
+    this.http.get('http://127.0.0.1:5000/getminmax?deposit=' + this.depositCurrency + '&receive=' + this.receiveCurrency).subscribe(
+      data => {
+        this.minMax = data;
+        console.log(data);
+      }
+    );
+  }
+  validateAddress(address) {
+    this.http.get('http://127.0.0.1:5000/validateaddress?symbol=eth&address=' + address).subscribe(
+      data => {
+        this.addressValidation = data;
+        console.log(data);
+      }
+    );
   }
 
   convert() {
@@ -81,13 +91,28 @@ export class BuyCryptoComponent implements OnInit {
   // }
 
 
+  constructor(private http: HttpClient) {
+  }
 
-  constructor(private http: HttpClient) {  }
-
-ngOnInit(): void {
+  ngOnInit(): void {
     this.http.get('http://127.0.0.1:5000/login').subscribe(
       (data) => {
         this.coinList1 = data;
+        this.depositCurrency = this.coinList1[0].symbol;
+        console.log(this.depositCurrency);
+      }
+    );
+    this.http.get('http://127.0.0.1:5000/currencypair?symbol=abt').subscribe(
+      data => {
+        this.coinList2 = data;
+        this.receiveCurrency = this.coinList2[0].symbol;
+        console.log(this.receiveCurrency);
+      }
+    );
+    this.http.get('http://127.0.0.1:5000/getminmax?deposit=' + this.depositCurrency + '&receive=' + this.receiveCurrency).subscribe(
+      data => {
+        this.minMax = data;
+        console.log(data);
       }
     );
   }
